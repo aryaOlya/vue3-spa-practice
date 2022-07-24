d1ea5f458b81731a04f5e007cc779b7fe06c0f74
arya olya <99118866+aryaOlya@users.noreply.github.com>
2022-07-24 15:37:36 +0430

    WIP

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import route from "./router/index"
+import 'bootstrap/dist/js/bootstrap.bundle.min'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(route)
+app.mount('#app')
