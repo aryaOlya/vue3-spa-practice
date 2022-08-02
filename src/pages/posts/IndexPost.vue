1a341f9b7d91e7411c28f512728c50bac454e219
arya olya <99118866+aryaOlya@users.noreply.github.com>
2022-08-02 17:44:37 +0430

    make number of posts dynamic

--- src/pages/posts/IndexPost.vue
+++ src/pages/posts/IndexPost.vue
@@ -7,6 +7,15 @@
   </section>
 
   <router-link class="btn btn-primary" :to="{name:'createPost'}">Add Post</router-link>
+
+  <select v-model="selected" @change="userReq" class="form-control" >
+    <option disabled value="">choose number of posts</option>
+    <option value="10">10 Posts</option>
+    <option value="20">20 Posts</option>
+    <option value="50">50 Posts</option>
+    <option value="100">All Posts</option>
+  </select>
+<!--  <button @click="userReq">filter</button>-->
   <section v-if="!loading" class="col-4" v-for="post in posts" :key="post.id">
     <Card :post="post" />
   </section>
@@ -16,22 +25,27 @@
 
 <script setup>
 import axios from 'axios'
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import Card from "@/components/posts/Card";
 
 let loading = ref(true)
 let posts = ref([])
+let selected =ref('10')
 
-function userReq(){
-  axios.get('https://jsonplaceholder.typicode.com/posts')
+ function userReq(){
+  axios.get(`https://jsonplaceholder.typicode.com/posts?_limit=${selected.value}`)
       .then(response=> {
         posts.value = response.data
         loading.value=false
+        console.log('response',response)
       })
       .catch(()=>console.log('error'))
 }
 
+
+
 userReq()
+
 
 </script>
 
